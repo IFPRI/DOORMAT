bcc1b45122fb3869d8e25186cb6f099a5a964195
abhimishr <A.Mishra@cgiar.org>
2023-03-20 16:30:19 -0400

    update namespace

--- R/buildPackage.R
+++ R/buildPackage.R
@@ -25,7 +25,7 @@ buildPackage <- function(lib = ".", quiet = FALSE){
                          check_dir = tempdir(),
                          cran = FALSE,
                          force_suggests = TRUE,
-                         quiet = FALSE)
+                         quiet = quiet)
   errors <- !is_empty(check_package$errors)
   warns  <- !is_empty(check_package$warnings)
   notes  <- !is_empty(check_package$notes)
@@ -42,7 +42,7 @@ buildPackage <- function(lib = ".", quiet = FALSE){
     user_promt <- menu(c("Yes", "No"), title="Would you like to install this package on your computer?")
     if(user_promt==1) {
       use_version()
-      pkg_install(pkg=".")
+      pkg_install(pkg=lib)
       }
   }
 }
